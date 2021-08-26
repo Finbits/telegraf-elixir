0b166dcd0dcf939fc8e5998fae1b877d54462632
Thiago Santos <thia.mdossantos@gmail.com>
2021-08-26 17:41:27 -0300

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,31 +1,60 @@
 defmodule Telegraf.MixProject do
   use Mix.Project
 
+  @name "Telegraf"
+  @version "0.0.1-dev"
+  @description "Send metrics to telegraf"
+  @repo_url "https://github.com/Finbits/telegraf-elixir"
+
+
   def project do
     [
       app: :telegraf,
-      version: "0.1.0",
-      elixir: "~> 1.12",
-      start_permanent: Mix.env() == :prod,
+      version: @version,
+      elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
-      deps: deps()
+      name: @name,
+      description: @description,
+      deps: deps(),
+      docs: docs(),
+      package: package(),
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:nimble_pool, "~> 0.2"}
+      {:nimble_pool, "~> 0.2"},
+
+      # dev/test
+      {:excoveralls, "~> 0.14", only: :test}
     ]
+  end
+
+  defp docs do
+    [
+      main: @name,
+      source_ref: "v#{@version}",
+      source_url: @repo_url,
+      skip_undefined_reference_warnings_on: ["CHANGELOG.md"],
+      extras: ["CHANGELOG.md"]
+    ]
+  end
+
+  defp package do
+    %{
+      licenses: ["Apache-2.0"],
+      maintainers: ["Finbits"],
+      links: %{"GitHub" => @repo_url}
+    }
   end
 end
